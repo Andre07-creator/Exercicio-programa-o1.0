24df2346f62f753f8ffa06f13db355cf08b5a650
Andre <aramaldes876@gmail.com>
2020-07-13 20:02:01 -0300

    Adicionado classe ao programa V1.1

---
+++ ConsoleApp1/Entites/quant_atendido.cs
@@ -0,0 +1,60 @@
+﻿namespace ConsoleApp1.Entites
+{
+    class Quant_atendido
+    {
+        public int A1 { get; set; }
+        public int B1 { get; set; }
+        public int C1 { get; set; }
+
+        public int A { get; set; }
+        public int B { get; set; }
+        public int C { get; set; }
+        public int Total { get; set; }
+        public Quant_atendido()
+        {
+
+        }
+        public Quant_atendido(int a1, int b1, int c1, int a, int b, int c)
+        {
+            A1 = a1;
+            B1 = b1;
+            C1 = c1;
+            A = a;
+            B = b;
+            C = c;
+        }
+
+        public bool Calculo_Atendi()
+        {
+            if (0 <= A && A <= 100 && 0 <= B && B <= 100 && 0 <= C && C <= 100 && 0 <= A1 && A1 <= 100 && 0 <= B1 && B1 <= 100 && 0 <= C1 && C1 <= 100)
+            {
+
+                int total = 0;
+
+                if (A > A1)
+                {
+                    total += (A - A1);
+                }
+
+                if (B > B1)
+                {
+                    total += (B - B1);
+                }
+
+                if (C > C1)
+                {
+                    total += (C - C1);
+                }
+                Total = total;
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
+
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ConsoleApp1.Entites;
+using System;
 
 namespace ConsoleApp1
 {
@@ -10,47 +7,26 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("80, 20, 40");
             string[] vetor1 = Console.ReadLine().Split(' ');
-            int A1 = int.Parse(vetor1[0]);
-            int B1 = int.Parse(vetor1[1]);
-            int C1 = int.Parse(vetor1[2]);
+            int a1 = int.Parse(vetor1[0]);
+            int b1 = int.Parse(vetor1[1]);
+            int c1 = int.Parse(vetor1[2]);
 
             string[] vetor = Console.ReadLine().Split(' ');
-            int A = int.Parse(vetor[0]);
-            int B = int.Parse(vetor[1]);
-            int C = int.Parse(vetor[2]);
-            int Total = 0;
+            int a = int.Parse(vetor[0]);
+            int b = int.Parse(vetor[1]);
+            int c = int.Parse(vetor[2]);
+            Quant_atendido qtd_a = new Quant_atendido(a1, b1, c1, a, b, c);
 
-           
 
-            if (0 <= A && A <= 100 && 0 <= B && B <= 100 && 0 <= C && C <= 100 && 0 <= A1 && A1 <= 100 && 0 <= B1 && B1 <= 100 && 0 <= C1 && C1 <= 100)
+            if (qtd_a.Calculo_Atendi())
             {
-                //A += 80;
-                //B += 20;
-                //C += 40;
-                if (A > A1)
-                {
-                    Total += (A - A1); 
-                }
-
-                if (B > B1)
-                {
-                    Total += (B - B1);
-                }
-
-                if (C > C1)
-                {
-                    Total += (C - C1);
-                }
-                Console.WriteLine(Total);
+                Console.WriteLine(qtd_a.Total);
             }
-
             else
             {
-                Console.WriteLine("Números invalidos!");
+                Console.WriteLine("Algum número não corresponde aos valores pre-determinados!");
             }
-
         }
     }
 }
